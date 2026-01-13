de73310a4d94a6ee3bb8440a9f9bc0e63f6d155d
ilham <ilhammulya.cs@gmail.com>
2026-01-13 17:03:11 +0700

    Auth

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -3,30 +3,40 @@ import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 class AutTest(unittest.TestCase):
 
     def setUp(self):
-        options = webdriver.FirefoxOptions()
+        # Ambil browser dari argumen ke-2. Default ke 'firefox'
+        browser_type = sys.argv[2] if len(sys.argv) > 2 else "firefox"
+        
+        # Logika pemilihan Options
+        if browser_type == "chrome":
+            options = webdriver.ChromeOptions()
+        elif browser_type == "edge":
+            options = webdriver.EdgeOptions()
+        else:
+            options = webdriver.FirefoxOptions()
+
+        # Konfigurasi umum (SSL ignore)
         options.add_argument('--ignore-ssl-errors=yes')
         options.add_argument('--ignore-certificate-errors')
+        
+        server = 'http://localhost:4444'
 
-        server = "http://localhost:4444"
         self.browser = webdriver.Remote(command_executor=server, options=options)
         self.addCleanup(self.browser.quit)
 
     def test_homepage(self):
+        # Ambil URL dari argumen ke-1
         if len(sys.argv) > 1:
             url = sys.argv[1]
         else:
-            url = "http://docker-apache"   # ✅ ini yang benar
+            url = "http://localhost"
 
         self.browser.get(url)
-
         expected_result = "Welcome back, Guest!"
         actual_result = self.browser.find_element(By.TAG_NAME, 'p')
         self.assertIn(expected_result, actual_result.text)
-
 
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], verbosity=2, warnings='ignore')
