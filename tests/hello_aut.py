00844b501421b3704d9174fc528ca186d11b632b
ilham <ilhammulya.cs@gmail.com>
2026-01-13 17:07:30 +0700

    this auth

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -1,42 +1,43 @@
-import unittest
-import sys
+import unittest, sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 class AutTest(unittest.TestCase):
 
     def setUp(self):
-        # Ambil browser dari argumen ke-2. Default ke 'firefox'
-        browser_type = sys.argv[2] if len(sys.argv) > 2 else "firefox"
-        
-        # Logika pemilihan Options
-        if browser_type == "chrome":
+        browser = sys.argv[2] if len(sys.argv) > 2 else "firefox"
+
+        if browser == "chrome":
             options = webdriver.ChromeOptions()
-        elif browser_type == "edge":
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+        elif browser == "edge":
             options = webdriver.EdgeOptions()
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
         else:
             options = webdriver.FirefoxOptions()
 
-        # Konfigurasi umum (SSL ignore)
-        options.add_argument('--ignore-ssl-errors=yes')
-        options.add_argument('--ignore-certificate-errors')
-        
-        server = 'http://localhost:4444'
+        options.add_argument("--headless")
+        options.add_argument("--ignore-certificate-errors")
 
-        self.browser = webdriver.Remote(command_executor=server, options=options)
+        self.browser = webdriver.Remote(
+            command_executor="http://localhost:4444",
+            options=options
+        )
         self.addCleanup(self.browser.quit)
 
+
     def test_homepage(self):
-        # Ambil URL dari argumen ke-1
-        if len(sys.argv) > 1:
-            url = sys.argv[1]
-        else:
-            url = "http://localhost"
+        url = sys.argv[1] if len(sys.argv) > 1 else "http://localhost"
 
         self.browser.get(url)
+        self.browser.save_screenshot("screenshot.png")
         expected_result = "Welcome back, Guest!"
-        actual_result = self.browser.find_element(By.TAG_NAME, 'p')
+        actual_result = self.browser.find_element(By.TAG_NAME, "p")
+
         self.assertIn(expected_result, actual_result.text)
 
-if __name__ == '__main__':
-    unittest.main(argv=['first-arg-is-ignored'], verbosity=2, warnings='ignore')
+
+if name == "main":
+    unittest.main(argv=["first-arg-is-ignored"], verbosity=2, warnings="ignore")
