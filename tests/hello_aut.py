d4323d165eb0f540a94ba259499b25fb0f5bbced
ilham <ilhammulya.cs@gmail.com>
2026-01-13 17:18:49 +0700

    Auth nih

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -1,43 +1,34 @@
-import unittest, sys
+import unittest
+import sys
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 class AutTest(unittest.TestCase):
 
     def setUp(self):
-        browser = sys.argv[2] if len(sys.argv) > 2 else "firefox"
+        options = webdriver.FirefoxOptions()
+        options.add_argument('--ignore-ssl-errors=yes')
+        options.add_argument('--ignore-certificate-errors')
+        server = "http://localhost:4444"
 
-        if browser == "chrome":
-            options = webdriver.ChromeOptions()
-            options.add_argument("--no-sandbox")
-            options.add_argument("--disable-dev-shm-usage")
-        elif browser == "edge":
-            options = webdriver.EdgeOptions()
-            options.add_argument("--no-sandbox")
-            options.add_argument("--disable-dev-shm-usage")
-        else:
-            options = webdriver.FirefoxOptions()
-
-        options.add_argument("--headless")
-        options.add_argument("--ignore-certificate-errors")
-
-        self.browser = webdriver.Remote(
-            command_executor="http://localhost:4444",
-            options=options
-        )
+        self.browser = webdriver.Remote(command_executor=server, options=options)
         self.addCleanup(self.browser.quit)
 
-
     def test_homepage(self):
-        url = sys.argv[1] if len(sys.argv) > 1 else "http://localhost"
+        if len(sys.argv) > 1:
+            url = sys.argv[1]
+        else:
+            url = "http://docker-apache"
+
+        # Ensure proper URL format
+        if not url.startswith('http'):
+            url = "http://" + url
 
         self.browser.get(url)
-        self.browser.save_screenshot("screenshot.png")
         expected_result = "Welcome back, Guest!"
-        actual_result = self.browser.find_element(By.TAG_NAME, "p")
+        actual_result = self.browser.find_element(By.TAG_NAME, 'p').text
+        self.assertIn(expected_result, actual_result)
 
-        self.assertIn(expected_result, actual_result.text)
-
-
-if __name__ == "__main__":
-    unittest.main(argv=["first-arg-is-ignored"], verbosity=2, warnings="ignore")
+if name == 'main':
+    unittest.main(argv=['first-arg-is-ignored'], verbosity=2, warnings='ignore')
