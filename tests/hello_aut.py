a471be64864565ff634758b5e83fd9291161b0a9
ilham <ilhammulya.cs@gmail.com>
2026-01-13 16:58:57 +0700

    Aut

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -11,34 +11,22 @@ class AutTest(unittest.TestCase):
         options.add_argument('--ignore-ssl-errors=yes')
         options.add_argument('--ignore-certificate-errors')
 
-        # Selenium Hub
         server = "http://localhost:4444"
-
-        self.browser = webdriver.Remote(
-            command_executor=server,
-            options=options
-        )
+        self.browser = webdriver.Remote(command_executor=server, options=options)
         self.addCleanup(self.browser.quit)
 
     def test_homepage(self):
-        # Ambil URL dari argument workflow
         if len(sys.argv) > 1:
             url = sys.argv[1]
         else:
-            # DEFAULT YANG BENAR UNTUK DOCKER NETWORK
-            url = "http://localhost"
+            url = "http://docker-apache"   # ✅ ini yang benar
 
         self.browser.get(url)
 
         expected_result = "Welcome back, Guest!"
         actual_result = self.browser.find_element(By.TAG_NAME, 'p')
-
         self.assertIn(expected_result, actual_result.text)
 
 
 if __name__ == '__main__':
-    unittest.main(
-        argv=['first-arg-is-ignored'],
-        verbosity=2,
-        warnings='ignore'
-    )
+    unittest.main(argv=['first-arg-is-ignored'], verbosity=2, warnings='ignore')
