5d5417a8842af1394041345c7fe7e25638e55fa6
ilham <ilhammulya.cs@gmail.com>
2026-01-13 20:05:29 +0700

    Screenshoot

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -12,26 +12,24 @@ class AutTest(unittest.TestCase):
         options.add_argument('--ignore-certificate-errors')
         server = "http://localhost:4444"
 
-        # Konfigurasi umum (SSL ignore)
-        options.add_argument('--ignore-ssl-errors=yes')
-        options.add_argument('--ignore-certificate-errors')
-        
-        server = 'http://localhost:4444'
-
         self.browser = webdriver.Remote(command_executor=server, options=options)
         self.addCleanup(self.browser.quit)
 
     def test_homepage(self):
-        # Ambil URL dari argumen ke-1
         if len(sys.argv) > 1:
             url = sys.argv[1]
         else:
-            url = "http://localhost"
+            url = "http://docker-apache"
+
+        # Ensure proper URL format
+        if not url.startswith('http'):
+            url = "http://" + url
 
         self.browser.get(url)
+        self.browser.save_screenshot("screenshot.png")
         expected_result = "Welcome back, Guest!"
-        actual_result = self.browser.find_element(By.TAG_NAME, 'p')
-        self.assertIn(expected_result, actual_result.text)
+        actual_result = self.browser.find_element(By.TAG_NAME, 'p').text
+        self.assertIn(expected_result, actual_result)
 
-if __name__ == '__main__':
+if name == 'main':
     unittest.main(argv=['first-arg-is-ignored'], verbosity=2, warnings='ignore')
